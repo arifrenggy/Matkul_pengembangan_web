e89b624e9b1bed98e3ddad5265f191039b7b81a8
Arif Renggy <arifrenggy00@gmail.com>
2025-10-09 05:10:09 +0800

    update tugas/SoalPerulangan1.php

--- tugas/SoalPerulangan1.php
+++ tugas/SoalPerulangan1.php
@@ -2,12 +2,10 @@
 $baju = 80000;
 $celana = 100000;
 $diskon = [3, 6, 9, 12];
-//indeks dari 0
 
 for ($i = 1; $i <= 4; $i+) {
   $BajuDiskon = $baju - ($baju * $diskon[$i - 1]) / 100;
   $celanaDiskon = $celana - ($celana * $diskon[$i - 1]) / 100;
-  // - 1 di tambhakn untuk menyesuailan indeks 0 mendjadi 1
   $totalDiskon = $BajuDiskon + $celanaDiskon;
 
   echo "hari ke- $i <br>";
